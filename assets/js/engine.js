823c537eac069d3044047993df6c241a0d7fa093
jackashen <db@Pablo-Ganem-2.local>
2013-10-28 10:25:44 -0600

    busqueda

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -43,33 +43,22 @@ $(document).ready(function() {
 	
 	$(document).on("ready", main);
 	
-		function main(){
-			$("#tfBuscar").on("keyup", buscar);
-		}
 		function buscar(){
-			var tarjetas = $(".tarjeta");
-			var texto = $("#tfBuscar").val();
-			texto = texto.toLowerCase();
-			tarjetas.show();
-			for(var i=0; i< tarjetas.size(); i++){
-				var contenido = tarjetas.eq(i).text();
-				contenido = contenido.toLowerCase();
-				var index = contenido.indexOf(texto);
-				if(index = -1){
-					tarjetas.eq(i).hide();
-				
-					
-				}
-				if(index = ""){
-				$("#results").html(" hola")
-				}
-			}
-		}
-
-
-	
-	
-	
+	var tarjetas = $(".tarjeta");
+	var texto    = $("#tfBuscar").val();
+	   $("#tfBuscar").focus();
+	texto        = texto.toLowerCase();
+	tarjetas.show();
+	for(var i=0; i< tarjetas.size(); i++){
+		var contenido = tarjetas.eq(i).text();
+		contenido     = contenido.toLowerCase();
+		var index     = contenido.indexOf(texto);
+		 
+		if(index == -1){
+			tarjetas.eq(i).hide();
+		}		
+	}
+}
 
 });
 
